8b496c6adc427850a03cfef86004c1b70c9e3b78
gingray <gingray.dev@gmail.com>
2016-11-29 00:03:14 +0300

    change deploy preference

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,6 @@
-role :app, %w(oceanorentals@184.168.146.56)
+role :app, %w(haciendabnb@184.168.146.56)
 
 set :pty, true
 
-set :user, 'oceanorentals'
-set :password, 'CarlaHale+1'
-set :deploy_to, -> { "/home/oceanorentals/app" }
+set :user, 'haciendabnb'
+set :deploy_to, -> { "/home/haciendabnb/app" }
