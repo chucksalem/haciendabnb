04b16f31f2d05e85d029c38ac89f129303833342
gingray <gingray.dev@gmail.com>
2016-11-30 02:17:41 +0300

    fix bug

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,10 @@
 threads 0,8
 workers 2
 preload_app!
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+
 bind "unix://#{shared_dir}/tmp/puma.sock"
 
 daemonize true
